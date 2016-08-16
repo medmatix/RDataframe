dfd456a6c68e4a128511a3727b6e5593ce4af1f3
David York <david@debian2x8david>
2016-08-16 19:03:20 -0400

    retry

---
+++ docs/html/search/all_8.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['r_2ddataframe',['R-Dataframe',['../index.html',1,'']]],
+  ['rdataframe07019_2ecxx',['RDataframe07019.cxx',['../_r_dataframe07019_8cxx.html',1,'']]]
+];
---
+++ docs/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['code_20for_20project',['Code for Project',['../md__r_dataframe_readme.html',1,'']]]
+];
